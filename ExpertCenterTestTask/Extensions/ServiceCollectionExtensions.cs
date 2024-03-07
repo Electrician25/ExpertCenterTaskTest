0944d492fb806ab3b0a91f854b1e04820f03cd2a
Kirill <sosisks888@mail.ru>
2024-03-07 11:36:30 +0300

    refactoring code

--- ExpertCenterTestTask/Extensions/ServiceCollectionExtensions.cs
+++ ExpertCenterTestTask/Extensions/ServiceCollectionExtensions.cs
@@ -7,8 +7,8 @@ namespace ExpertCenterTestTask.Extensions
 		public static IServiceCollection AddCategoryCrudServices(this IServiceCollection services)
 		{
 			services.AddTransient<PriceListsCrud>();
-			services.AddTransient<ColumnCrud>();
 			services.AddTransient<ColumnDataCrud>();
+			services.AddTransient<ColumnCrud>();
 			services.AddTransient<ItemCrud>();
 
 			return services;
--- ExpertCenterTestTask/Notifiers/Notifier.cs
+++ ExpertCenterTestTask/Notifiers/Notifier.cs
@@ -5,16 +5,17 @@ namespace ExpertCenterTestTask.Notifiers;
 
 public abstract class Notifier
 {
-    public const string ReceiveClientMethodName = "Receive";
-    protected readonly IHubContext<NotificationHub> HubContext;
+	public const string ReceiveClientMethodName = "Receive";
+	protected readonly IHubContext<NotificationHub> HubContext;
 
-    public Notifier(IHubContext<NotificationHub> hubContext)
-    {
-        HubContext = hubContext;
-    }
+	public Notifier(IHubContext<NotificationHub> hubContext)
+	{
+		HubContext = hubContext;
+	}
 
-    public abstract string GroupName { get; }
+	public abstract string GroupName { get; }
 
-    protected async Task Notify<T> (string dataType, T data)
-        => await HubContext.Clients.Group(GroupName).SendAsync(ReceiveClientMethodName, GroupName, dataType, data);
+	protected async Task Notify<T>(string dataType, T data)
+		=> await HubContext.Clients.Group(GroupName)
+			.SendAsync(ReceiveClientMethodName, GroupName, dataType, data);
 }
